540739ff9937739f455ad3a3684696f0faba3d0e
Tim Dronkers <tdronkers@ascendanalytics.com>
2026-03-03 15:25:24 -0700

    add telemetry

--- src/agent/run.ts
+++ src/agent/run.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config'
 import { openai } from '@ai-sdk/openai'
+import { getTracer, Laminar } from '@lmnr-ai/lmnr'
 import { generateText, type ModelMessage } from 'ai'
 import type { AgentCallbacks } from '../types'
 import { executeTool } from './executeTools'
@@ -7,6 +8,10 @@ import { SYSTEM_PROMPT } from './system/prompt'
 import { type ToolName, tools } from './tools'
 
 const MODEL_NAME = 'gpt-5-mini'
+
+Laminar.initialize({
+	projectApiKey: process.env.LMNR_PROJECT_API_KEY,
+})
 
 export const runAgent = async (
 	userMessage: string,
@@ -18,15 +23,26 @@ export const runAgent = async (
 		system: SYSTEM_PROMPT,
 		prompt: userMessage,
 		tools,
-		toolChoice: 'auto', // auto by default
-		activeTools: ['dateTime'],
+		experimental_telemetry: {
+			isEnabled: true,
+			tracer: getTracer(),
+		},
 	})
+
+	await Laminar.flush()
 
 	for await (const tc of toolCalls) {
 		const toolName = tc.toolName as ToolName
 		const tcResult = await executeTool(toolName)
 		console.log('tcResult: ', tcResult) //  2026-02-13T20:47:57.681Z
 	}
+
+	return text
 }
 
-runAgent('What is the current time in Denver, Colorado?')
+const program = async (prompt: string) => {
+	const result = await runAgent(prompt)
+	console.log('result: ', result)
+}
+
+program('when does daylight savings take place in the USA Mountain time zone?')
